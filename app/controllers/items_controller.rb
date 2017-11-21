b48059afadfa6fef1f45f41b052bc592c14d56e8
Tim DiSabatino <tim@pathwaysdigital.com>
2017-11-21 10:34:10 -0800

    Index, New & Show actions & views

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,11 @@
 class ItemsController < ApplicationController
+  before_action :find_item, only: [:show, :edit, :update, :destroy]
+  
   def index
+    @items = Item.all.order("created_at DESC")
+  end
+
+  def show
   end
 
   def new
@@ -7,5 +13,22 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).
+      permit(:title, :description)
+  end
+
+  def find_item
+    @item = Item.find(params[:id])
   end
 end
