3c29eeceb8b92e3336ce615f8f4ff21d53faeccf
Tim DiSabatino <tim@pathwaysdigital.com>
2017-11-21 14:28:28 -0800

    Add edit, update & destroy functionality

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :find_item, only: [:show, :edit, :update, :destroy]
-  
+
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -19,6 +19,22 @@ class ItemsController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
   end
 
   private
